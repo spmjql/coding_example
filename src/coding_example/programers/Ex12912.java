45006be4c652e1e0f992dabdec949c9f79b64984
spmjql <141596791+spmjql@users.noreply.github.com>
2024-04-02 22:28:10 +0900

    두 정수 사이의 합, 행렬의 덧셈

---
+++ src/coding_example/programers/Ex12912.java
@@ -0,0 +1,17 @@
+package coding_example.programers;
+
+public class Ex12912 {
+    public long solution(int a, int b) {
+        long answer = 0;
+        long min = Math.min(a, b);
+        for(; min<=Math.max(a, b); min++) {
+        	answer += min;
+        }
+        return answer;
+    }
+}
+/*
+두 정수 사이의 합
+두 정수 a, b가 주어졌을 때 a와 b 사이에 속한 모든 정수의 합을 리턴하는 함수, solution을 완성하세요.
+예를 들어 a = 3, b = 5인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다. 
+*/
