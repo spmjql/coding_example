78fe75661afabfba9538133780aa682c0d754f3f
spmjql <141596791+spmjql@users.noreply.github.com>
2024-04-07 23:55:46 +0900

    문자열 다루기 기본, 숫자 자릿수 더하기

---
+++ src/coding_example/programers/Ex12931.java
@@ -0,0 +1,14 @@
+package coding_example.programers;
+
+import java.util.*;
+
+public class Ex12931 {
+    public int solution(int n) {
+        int answer = 0;
+        while(n!=0) {
+        	answer += n%10;
+        	n /= 10;
+        }
+        return answer;
+    }
+}
